337ba27012509a6872a162ff1c89f6b480572ad2
unknown <williamrusdyputra@gmail.com>
2024-01-17 00:47:02 +0700

    feat: add footer

---
+++ frontend/components/footer.tsx
@@ -0,0 +1,13 @@
+const Footer = () => {
+    return (
+        <footer className="bg-footer">
+          <div className="py-4 items-center justify-center flex">
+            <p className="text-gray-600">
+                &copy; 2023 Pixel8Labs. All rights reserved.
+            </p>
+        </div>
+      </footer>
+    );
+}
+
+export default Footer;
--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -1,8 +1,14 @@
 import type { NextPage } from 'next';
 import Header from '@/components/header';
+import Footer from '@/components/footer';
 
 const Home: NextPage = () => {
-  return <Header />;
+  return (
+    <div className="flex flex-col h-screen justify-between">
+      <Header />
+      <Footer />
+    </div>
+  );
 };
 
 export default Home;
